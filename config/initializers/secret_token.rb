e08d135bbf8116dbbde18320737647ace2ec3a94
mmehrotra <mudit.mehrotra@triffort.com>
2013-05-09 01:07:11 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CassidyApp::Application.config.secret_token = '4803b059602aa87925b657da2a40aac9e713a76f66dfbc8167dda0bc630193b148dabda1e9aa0f6ca773847f6ceb67f35cb90aec836a0ae0dc73b6f11c741380'
